87027a445e74d52c56f1ed30433f9f2182b7b340
chaitali-narkhede <chairali.narkhede1991@gmail.com>
2021-06-06 00:03:17 +0530

    deleted angular project

--- Angylar12Project/src/app/contact-details/_models/contact-details.model.ts
+++
@@ -1,7 +0,0 @@
-export class ContactDetailsModel {
-  public contactId: number = 0;
-  public firstName: string = "";
-  public lastName: string = "";
-  public email: string = "";
-  public phoneNumber: string = "";
-}
--- Angylar12Project/src/app/models/alerts.ts
+++
@@ -1,6 +0,0 @@
-export const RecordSaved = "Record saved successfully";
-export const RecordUpdated = "Record updated successfully";
-export const RecordDeleted = "Record delete successfully";
-export const RecordNotSaved = "Record does not saved";
-export const RecordNotUpdated = "Record does not updated";
-export const RecordNotDeleted = "Record does not deleted";
